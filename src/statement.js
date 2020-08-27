0ae86d04a9617b25387bf31e7363f641570f10dc
olivia-wang-code <2350625418@qq.com>
2020-08-27 19:04:01 +0800

    [refactor-exercise][olivia]feature:add convertStatementToHtml function and pass

--- src/statement.js
+++ src/statement.js
@@ -50,7 +50,32 @@ function calulateVolumeCredits(volumeCredits, perf, play) {
   return result;
 }
 
+function convertStatementToHtml(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `<h1>Statement for ${invoice.customer}</h1>\n`+'<table>\n' +
+  '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+
+
+  //   '<tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+  //   '<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+  //   '<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+  //   '</table>\n' +
+  //   '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+  //   '<p>You earned <em>47</em> credits</p>\n');
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = caculateThisAmount(perf, play);
+    volumeCredits = calulateVolumeCredits(volumeCredits, perf, play);
+    result += `<tr><td>${play.name}</td><td>${perf.audience}</td><td>${format(thisAmount / 100)}</td></tr>\n`;
+    totalAmount += thisAmount;
+  }
+  result += `</table>\n<p>Amount owed is <em>${format(totalAmount / 100)}</em></p>\n`;
+  result += `<p>You earned <em>${volumeCredits}</em> credits</p>\n`;
+  return result;
+}
 
 module.exports = {
   statement,
+  convertStatementToHtml
 };
--- test/statementTest - 2.js
+++ test/statementTest - 2.js
@@ -1,5 +1,6 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const { statement, convertStatementToHtml } = require('../src/statement');
+
 
 const plays = {
   'hamlet': {
@@ -108,33 +109,33 @@ test('statement case 6. Customer BigCo has three performances. ' +
   'As You Like Is has 35 audiences. ' +
   'Othello has 40 audiences. ', t => {
 
-  const invoice = {
-    'customer': 'BigCo',
-    'performances': [
-      {
-        'playID': 'hamlet',
-        'audience': 55,
-      },
-      {
-        'playID': 'as-like',
-        'audience': 35,
-      },
-      {
-        'playID': 'othello',
-        'audience': 40,
-      },
-    ],
-  };
+    const invoice = {
+      'customer': 'BigCo',
+      'performances': [
+        {
+          'playID': 'hamlet',
+          'audience': 55,
+        },
+        {
+          'playID': 'as-like',
+          'audience': 35,
+        },
+        {
+          'playID': 'othello',
+          'audience': 40,
+        },
+      ],
+    };
 
-  const result = statement(invoice, plays);
+    const result = statement(invoice, plays);
 
-  t.is(result, 'Statement for BigCo\n' +
-    ' Hamlet: $650.00 (55 seats)\n' +
-    ' As You Like It: $580.00 (35 seats)\n' +
-    ' Othello: $500.00 (40 seats)\n' +
-    'Amount owed is $1,730.00\n' +
-    'You earned 47 credits \n');
-});
+    t.is(result, 'Statement for BigCo\n' +
+      ' Hamlet: $650.00 (55 seats)\n' +
+      ' As You Like It: $580.00 (35 seats)\n' +
+      ' Othello: $500.00 (40 seats)\n' +
+      'Amount owed is $1,730.00\n' +
+      'You earned 47 credits \n');
+  });
 
 test('statement case 7. Customer BigCo has one unknown performance. ', t => {
   const plays = {
@@ -161,3 +162,39 @@ test('statement case 7. Customer BigCo has one unknown performance. ', t => {
     t.is(e.message, 'unknown type: tragedy1');
   }
 });
+
+test('statement case 6. Customer BigCo has three performances. ' +
+  'Hamlet has 55 audiences. ' +
+  'As You Like Is has 35 audiences. ' +
+  'Othello has 40 audiences. ', t => {
+
+    const invoice = {
+      'customer': 'BigCo',
+      'performances': [
+        {
+          'playID': 'hamlet',
+          'audience': 55,
+        },
+        {
+          'playID': 'as-like',
+          'audience': 35,
+        },
+        {
+          'playID': 'othello',
+          'audience': 40,
+        },
+      ],
+    };
+
+    const result = convertStatementToHtml(invoice, plays);
+
+    t.is(result, '<h1>Statement for BigCo</h1>\n' +
+      '<table>\n' +
+      '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+      '<tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+      '<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+      '<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+      '</table>\n' +
+      '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+      '<p>You earned <em>47</em> credits</p>\n');
+  });
