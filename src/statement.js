cd7796aedcb7cbfdc3f6f0b493b618ad209f953f
olivia-wang-code <2350625418@qq.com>
2020-08-27 16:53:12 +0800

    [refactor-exercise][olivia]refactor:move const format to top of function

--- src/statement.js
+++ src/statement.js
@@ -1,14 +1,14 @@
 const { playType } = require('./constContainer')
+const format = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+}).format;
 
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = caculateThisAmount(perf, play);
